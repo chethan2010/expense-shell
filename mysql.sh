263867190609c7bab53cce8dc46a33495256ba0a
chethan2010 <hemanthkumarsaketi@gmail.com>
2024-12-01 05:51:58 +0530

    Expense-shell-script

--- mysql.sh
+++ mysql.sh
@@ -37,7 +37,7 @@ fi
 
 #    mysql_secure_installation --set-root-pass ExpenseApp@1
 #    VALIDATE $? "Setting up root password"
-   mysqlSQL -h db.daws93s.online -uroot  -p${mysql_root_password} -e 'SHOW DATABASES;' &&>>LOGFILE
+   mysql -h db.daws93s.online -uroot  -p${mysql_root_password} -e 'SHOW DATABASES;' &&>>LOGFILE
    if [ $? -ne 0 ]
    then
         mysql_secure_installation --set-root-pass ${read -s mysql_root_password} &>>LOGFILE
